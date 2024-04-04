dcb682dfea9464df41dfdc0e331af054a4dfc72c
vaishnavi-diya15 <vaishnavi.diya15@gmail.com>
2024-04-04 12:41:43 +0530

    main project commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RightComponent } from "./right/right.component";
+import { LeftComponent } from "./left/left.component";
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RightComponent, LeftComponent]
 })
 export class AppComponent {
   title = 'KnowYourWorld';
---
+++ src/app/right/right.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right',
+  standalone: true,
+  imports: [],
+  templateUrl: './right.component.html',
+  styleUrl: './right.component.css'
+})
+export class RightComponent {
+
+}
